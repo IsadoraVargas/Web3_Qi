89881159de89dfc23e8c38fab20f41564703c31b
IsadoraVargas <79876349+IsadoraVargas@users.noreply.github.com>
2021-11-04 16:42:13 -0300

    Delete Hospital.java

--- Hospital/lib/Hospital.java
+++
@@ -1,8 +0,0 @@
-package projetos.Hospital.lib;
-public class App{
-public static void main(String[] args) {
-  Paciente p1 = new Paciente();
-  Medico m1 = new Medico();
-  Internacao i1 = new Internacao();
-}
-}
